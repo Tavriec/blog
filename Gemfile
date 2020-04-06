21ffcd834883eba01e97e3f95ed48a70771093d1
Serhii Trushko <TrushkoSergey2@gmail.com>
2020-04-07 02:17:09 +0300

    Add database_cleaner

--- Gemfile
+++ Gemfile
@@ -46,8 +46,9 @@ group :test, :development do
   gem 'rspec-rails'
   gem 'shoulda-matchers'
   # gem 'capybara', '>= 2.15'
-  gem "factory_bot_rails"
-
+  gem 'factory_bot_rails'
+  gem 'database_cleaner-active_record'
+  gem 'database_cleaner-redis'
 end
 
 group :test do
@@ -55,7 +56,7 @@ group :test do
   gem 'capybara', '>= 2.15'
   gem 'selenium-webdriver'
   # Easy installation and use of web drivers to run system tests with browsers
-  gem 'webdrivers'
+  gem 'webdrivers'  
 end
 
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,6 +1,7 @@
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 require 'spec_helper'
 require 'session_helper'
+
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../config/environment', __dir__)
 # Prevent database truncation if the environment is production
@@ -38,7 +39,7 @@ RSpec.configure do |config|
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
   # instead of true.
-  config.use_transactional_fixtures = true
+  config.use_transactional_fixtures = false
 
   # You can uncomment this line to turn off ActiveRecord support entirely.
   # config.use_active_record = false
@@ -63,6 +64,7 @@ RSpec.configure do |config|
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
   config.include FactoryBot::Syntax::Methods
+  
 end
 
 Shoulda::Matchers.configure do |config|
@@ -71,4 +73,6 @@ Shoulda::Matchers.configure do |config|
     with.library :rails
   end
 end
+
+
 
